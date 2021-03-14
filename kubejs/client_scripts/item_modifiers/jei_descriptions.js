2c30b535f8732c301c1667a778a02fb3d34ea447
ModernGamingWorld <ModernGamingWorld@outlook.com>
2021-03-14 16:44:57 -0500

    Removing everything to clean up and start somethings over

--- kubejs/client_scripts/item_modifiers/jei_descriptions.js
+++
@@ -1,3 +0,0 @@
-onEvent('jei.information', event => {
-  event.add(''astralsorcery:stardust'', ['Obtained by left-clicking a Starmetal Ingot in-world.'])  event.add(''astralsorcery:stardust'', ['Obtained by left-clicking a Starmetal Ingot in-world.'])
-})
