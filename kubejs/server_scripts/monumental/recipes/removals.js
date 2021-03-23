6908116b8dca97a9c495abd6350163c2daaa046b
ModernGamingWorld <ModernGamingWorld@outlook.com>
2021-03-22 21:15:13 -0500

    Create removals.js

---
+++ kubejs/server_scripts/monumental/recipes/removals.js
@@ -0,0 +1,23 @@
+onEvent('recipes', event => {
+
+  //Iron Furnace upgrades recipes removed
+  event.remove({id: 'ironfurnaces:gold_furnace2'})
+  event.remove({id: 'ironfurnaces:iron_furnace2'})
+  event.remove({id: 'ironfurnaces:obsidian_furnace2'})
+  event.remove({id: 'ironfurnaces:silver_furnace2'})
+  event.remove({id: 'ironfurnaces:upgrade_iron'})
+  event.remove({id: 'ironfurnaces:upgrade_gold'})
+  event.remove({id: 'ironfurnaces:upgrade_diamond'})
+  event.remove({id: 'ironfurnaces:upgrade_emerald'})
+  event.remove({id: 'ironfurnaces:upgrade_obsidian'})
+  event.remove({id: 'ironfurnaces:upgrade_crystal'})
+  event.remove({id: 'ironfurnaces:upgrade_netherite'})
+  event.remove({id: 'ironfurnaces:upgrade_copper'})
+  event.remove({id: 'ironfurnaces:upgrade_silver'})
+  event.remove({id: 'ironfurnaces:upgrade_obsidian2'})
+  event.remove({id: 'ironfurnaces:upgrade_iron2'})
+  event.remove({id: 'ironfurnaces:upgrade_silver2'})
+  event.remove({id: 'ironfurnaces:upgrade_allthemodium'})
+  event.remove({id: 'ironfurnaces:upgrade_vibranium'})
+  event.remove({id: 'ironfurnaces:upgrade_unobtainium'})
+})
