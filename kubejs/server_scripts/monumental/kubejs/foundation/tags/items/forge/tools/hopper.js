bc00f56739f8bf9eb3f17597bd1bc0a90d10082b
ModernGamingWorld <ModernGamingWorld@outlook.com>
2021-04-05 23:44:12 -0500

    Create hopper.js

---
+++ kubejs/server_scripts/monumental/kubejs/foundation/tags/items/forge/tools/hopper.js
@@ -0,0 +1,18 @@
+events.listen('item.tags', (event) => {
+    var items = ['minecraft:hopper', 'naturesaura:grated_chute'];
+    var exceptions = [
+        'botaniafloating_hopperhook',
+        'botaniafloating_hopperhook_chibi',
+        'rsgauges:industrial_hopper_switch'
+    ];
+
+    var tags = ['forge:tools', 'forge:tools/hopper'];
+
+    tags.forEach((tag) => {
+        event
+            .get(tag)
+            .add(items)
+            .add(/_hopper/)
+            .remove(exceptions);
+    });
+});
