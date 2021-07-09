70a0f12e14e0ba759f67a6df838f780d7a3ed811
ModernGamingWorld <ModernGamingWorld@outlook.com>
2021-07-08 19:31:12 -0500

    Update induction_smelter.js

--- kubejs/server_scripts/Monumental/kubejs/foundation/recipetypes/thermal/machine/induction_smelter.js
+++ kubejs/server_scripts/Monumental/kubejs/foundation/recipetypes/thermal/machine/induction_smelter.js
@@ -6,6 +6,26 @@ onEvent('recipes', (event) => {
                 outputs: [
                     Item.of('refinedstorage:quartz_enriched_iron', 2)
                 ]
+            },
+            {
+                inputs: ['#forge:ingots/iron', '#forge:ingots/lead'],
+                outputs: [Item.of('eidolon:pewter_ingot', 2)]
+            },
+            {
+                inputs: ['#forge:ingots/iron', '#forge:dusts/ender'],
+                outputs: [Item.of('betterendforge:terminite_ingot')]
+            },
+            {
+                inputs: ['#forge:ingots/thallasium', '#forge:dusts/ender'],
+                outputs: [Item.of('betterendforge:terminite_ingot')]
+            },
+            {
+                inputs: ['#forge:ingots/netherite', 'betterendforge:terminite_ingot'],
+                outputs: [Item.of('betterendforge:aeternium_ingot')]
+            },
+            {
+                inputs: ['byg:quartzite_sand'],
+                outputs: [Item.of('minecraft:quartz'), Item.of('thermal:slag')]
             }
         ]
     };
