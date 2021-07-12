4968015955677d2cf9338c4aa4255807028552c4
ModernGamingWorld <ModernGamingWorld@outlook.com>
2021-07-12 12:29:54 -0500

    Update replace_input.js

--- kubejs/server_scripts/Monumental/kubejs/foundation/recipes/replace_input.js
+++ kubejs/server_scripts/Monumental/kubejs/foundation/recipes/replace_input.js
@@ -3,4 +3,9 @@ onEvent('recipes', (event) => {
   event.replaceInput({type: 'minecraft:crafting_shaped'}, 'minecraft:crafting_table', '#forge:workbenches')
   event.replaceInput({type: 'minecraft:crafting_shaped'}, 'minecraft:hopper', '#forge:hopper')
   event.replaceInput({type: 'minecraft:crafting_shaped'}, 'minecraft:obsidian', '#chipped:obsidian')
+  event.replaceInput({type: 'minecraft:crafting_shaped'}, 'minecraft:quartz_block', '#chipped:quartz_block')
+  event.replaceInput({type: 'minecraft:crafting_shaped'}, 'minecraft:nether_bricks', '#chipped:nether_bricks')
+  event.replaceInput({type: 'minecraft:crafting_shaped'}, 'minecraft:prismarine', '#chipped:prismarine')
+  event.replaceInput({type: 'minecraft:crafting_shaped'}, 'minecraft:purpur_block', '#chipped:purpur_block')
+  event.replaceInput({type: 'minecraft:crafting_shaped'}, 'minecraft:stone', '#chipped:stone')
 });
