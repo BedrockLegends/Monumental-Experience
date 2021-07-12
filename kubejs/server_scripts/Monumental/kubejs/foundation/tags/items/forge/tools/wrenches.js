47ca06d0974d1328edad349ace05e48e627f424d
ModernGamingWorld <ModernGamingWorld@outlook.com>
2021-07-11 19:49:21 -0500

    Update wrenches.js

--- kubejs/server_scripts/Monumental/kubejs/foundation/tags/items/forge/tools/wrenches.js
+++ kubejs/server_scripts/Monumental/kubejs/foundation/tags/items/forge/tools/wrenches.js
@@ -1,5 +1,10 @@
 onEvent('item.tags', (event) => {
-    event.add('forge:wrenches', [
-        'chiselsandbits:wrench_wood'
+    event.add('forge:tools/wrenches', [
+        'chiselsandbits:wrench_wood',
+        'routerreborn:itemwrench',
+        'appliedenergistics2:nether_quartz_wrench',
+        'appliedenergistics2:certus_quartz_wrench',
+        'create:wrench',
+        'cyclic:cable_wrench'
     ]);
 });
