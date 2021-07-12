0d9e944a4b0a1d7c271f0786d608972e31d4bfc4
ModernGamingWorld <ModernGamingWorld@outlook.com>
2021-07-11 19:33:59 -0500

    Update shaped.js

--- kubejs/server_scripts/Monumental/kubejs/foundation/recipetypes/minecraft/shaped.js
+++ kubejs/server_scripts/Monumental/kubejs/foundation/recipetypes/minecraft/shaped.js
@@ -1,4 +1,64 @@
 onEvent('recipes', event => {
+//Xnet
+event.shaped(Item.of('xnet:advanced_connector_blue', 1), [" A ", "ABA", " A "], {
+    A: '#forge:dyes/blue',
+    B: 'xnet:advanced_connector_routing'
+})
+event.shaped(Item.of('xnet:advanced_connector_green', 1), [" A ", "ABA", " A "], {
+    A: '#forge:dyes/green',
+    B: 'xnet:advanced_connector_routing'
+})
+event.shaped(Item.of('xnet:advanced_connector_red', 1), [" A ", "ABA", " A "], {
+    A: '#forge:dyes/red',
+    B: 'xnet:advanced_connector_routing'
+})
+event.shaped(Item.of('xnet:advanced_connector_yellow', 1), [" A ", "ABA", " A "], {
+    A: '#forge:dyes/yellow',
+    B: 'xnet:advanced_connector_routing'
+})
+event.shaped(Item.of('xnet:connector_blue', 1), [" A ", "ABA", " A "], {
+    A: '#forge:dyes/blue',
+    B: 'xnet:connector_routing'
+})
+event.shaped(Item.of('xnet:connector_green', 1), [" A ", "ABA", " A "], {
+    A: '#forge:dyes/green',
+    B: 'xnet:connector_routing'
+})
+event.shaped(Item.of('xnet:connector_red', 1), [" A ", "ABA", " A "], {
+    A: '#forge:dyes/red',
+    B: 'xnet:connector_routing'
+})
+event.shaped(Item.of('xnet:connector_yellow', 1), [" A ", "ABA", " A "], {
+    A: '#forge:dyes/yellow',
+    B: 'xnet:connector_routing'
+})
+event.shaped(Item.of('xnet:netcable_blue', 1), [" A ", "ABA", " A "], {
+    A: '#forge:dyes/blue',
+    B: 'xnet:netcable_routing'
+})
+event.shaped(Item.of('xnet:netcable_green', 1), [" A ", "ABA", " A "], {
+    A: '#forge:dyes/green',
+    B: 'xnet:netcable_routing'
+})
+event.shaped(Item.of('xnet:netcable_red', 1), [" A ", "ABA", " A "], {
+    A: '#forge:dyes/red',
+    B: 'xnet:netcable_routing'
+})
+event.shaped(Item.of('xnet:netcable_yellow', 1), [" A ", "ABA", " A "], {
+    A: '#forge:dyes/yellow',
+    B: 'xnet:netcable_routing'
+})
+event.shaped(Item.of('xnet:advanced_connector_routing', 1), ["AB ", "CD ", "   "], {
+    A: 'xnet:connector_routing',
+    B: 'minecraft:ender_eye',
+    C: '#forge:plates/diamond',
+    D: 'extendedcrafting:redstone_component'
+})
+event.shaped(Item.of('xnet:connector_routing', 1), ["AAA", "BCB", "AAA"], {
+    A: '#thermal:rockwool',
+    B: 'immersiveengineering:wirecoil_electrum',
+    C: 'xnet:netcable_routing'
+})
 //computercraft
 event.shaped(Item.of('computercraft:computer_advanced', 1), ["AAA", "ABA", "ACA"], {
     A: '#forge:plates/electrum',
@@ -16,6 +76,98 @@ event.shaped(Item.of('computercraft:turtle_normal', 1), ["AAA", "ABA", "ACA"], {
     C: 'immersiveengineering:reinforced_crate'
 })
 //Emendatus Enigmatica
+event.shaped(Item.of('emendatusenigmatica:diamond_plate', 1), ["   ", "BAA", " AA"], {
+    A: '#forge:gems/diamond',
+    B: '#forge:tools/wrenches',
+})
+event.shaped(Item.of('emendatusenigmatica:emerald_plate', 1), ["   ", "BAA", " AA"], {
+    A: '#forge:gems/emerald',
+    B: '#forge:tools/wrenches',
+})
+event.shaped(Item.of('emendatusenigmatica:lapis_plate', 1), ["   ", "BAA", " AA"], {
+    A: '#forge:gems/emerald',
+    B: '#forge:tools/wrenches',
+})
+event.shaped(Item.of('emendatusenigmatica:aluminum_plate', 1), ["   ", "BAA", " AA"], {
+    A: '#forge:ingots/aluminum',
+    B: '#forge:tools/wrenches',
+})
+event.shaped(Item.of('emendatusenigmatica:brass_plate', 1), ["   ", "BAA", " AA"], {
+    A: '#forge:ingots/brass',
+    B: '#forge:tools/wrenches',
+})
+event.shaped(Item.of('emendatusenigmatica:bronze_plate', 1), ["   ", "BAA", " AA"], {
+    A: '#forge:ingots/bronze',
+    B: '#forge:tools/wrenches',
+})
+event.shaped(Item.of('emendatusenigmatica:constantan_plate', 1), ["   ", "BAA", " AA"], {
+    A: '#forge:ingots/constantan',
+    B: '#forge:tools/wrenches',
+})
+event.shaped(Item.of('emendatusenigmatica:copper_plate', 1), ["   ", "BAA", " AA"], {
+    A: '#forge:ingots/copper',
+    B: '#forge:tools/wrenches',
+})
+event.shaped(Item.of('emendatusenigmatica:electrum_plate', 1), ["   ", "BAA", " AA"], {
+    A: '#forge:ingots/electrum',
+    B: '#forge:tools/wrenches',
+})
+event.shaped(Item.of('emendatusenigmatica:enderium_plate', 1), ["   ", "BAA", " AA"], {
+    A: '#forge:ingots/enderium',
+    B: '#forge:tools/wrenches',
+})
+event.shaped(Item.of('emendatusenigmatica:gold_plate', 1), ["   ", "BAA", " AA"], {
+    A: '#forge:ingots/gold',
+    B: '#forge:tools/wrenches',
+})
+event.shaped(Item.of('emendatusenigmatica:invar_plate', 1), ["   ", "BAA", " AA"], {
+    A: '#forge:ingots/invar',
+    B: '#forge:tools/wrenches',
+})
+event.shaped(Item.of('emendatusenigmatica:iron_plate', 1), ["   ", "BAA", " AA"], {
+    A: '#forge:ingots/iron',
+    B: '#forge:tools/wrenches',
+})
+event.shaped(Item.of('emendatusenigmatica:lead_plate', 1), ["   ", "BAA", " AA"], {
+    A: '#forge:ingots/lead',
+    B: '#forge:tools/wrenches',
+})
+event.shaped(Item.of('emendatusenigmatica:lumium_plate', 1), ["   ", "BAA", " AA"], {
+    A: '#forge:ingots/lumium',
+    B: '#forge:tools/wrenches',
+});
+event.shaped(Item.of('emendatusenigmatica:nickel_plate', 1), ["   ", "BAA", " AA"], {
+    A: '#forge:ingots/nickel',
+    B: '#forge:tools/wrenches',
+})
+event.shaped(Item.of('emendatusenigmatica:osmium_plate', 1), ["   ", "BAA", " AA"], {
+    A: '#forge:ingots/osmium',
+    B: '#forge:tools/wrenches',
+})
+event.shaped(Item.of('emendatusenigmatica:signalum_plate', 1), ["   ", "BAA", " AA"], {
+    A: '#forge:ingots/signalum',
+    B: '#forge:tools/wrenches',
+})
+event.shaped(Item.of('emendatusenigmatica:silver_plate', 1), ["   ", "BAA", " AA"], {
+    A: '#forge:ingots/silver',
+    B: '#forge:tools/wrenches',
+})
+event.shaped(Item.of('emendatusenigmatica:steel_plate', 1), ["   ", "BAA", " AA"], {
+    A: '#forge:ingots/steel',
+    B: '#forge:tools/wrenches',
+})
+event.shaped(Item.of('emendatusenigmatica:tin_plate', 1), ["   ", "BAA", " AA"], {
+    A: '#forge:ingots/tin',
+    B: '#forge:tools/wrenches',
+})
+event.shaped(Item.of('emendatusenigmatica:uranium_plate', 1), ["   ", "BAA", " AA"], {
+    A: '#forge:ingots/uranium',
+    B: '#forge:tools/wrenches',
+})
+event.shaped(Item.of('emendatusenigmatica:zinc_plate', 1), ["   ", "BAA", " AA"], {
+    A: '#forge:ingots/zinc',
+    B: '#forge:tools/wrenches',
+})
 event.shaped(Item.of('emendatusenigmatica:aluminum_gear', 1), [" A ", "ABA", " A "], {
     A: '#forge:ingots/aluminum',
     B: 'create:andesite_alloy'
