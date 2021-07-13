de62eafca89a352e68e0f5c6a294a3eb665fa64b
ModernGamingWorld <ModernGamingWorld@outlook.com>
2021-07-12 21:42:08 -0500

    Update metal_press.js

--- kubejs/server_scripts/Monumental/kubejs/foundation/recipetypes/immersiveengineering/metal_press.js
+++ kubejs/server_scripts/Monumental/kubejs/foundation/recipetypes/immersiveengineering/metal_press.js
@@ -1,6 +1,6 @@
 onEvent('recipes', (event) => {
     const recipes = [
-        { output: 'mekanism:hdpe_sheet', input: 'mekanism:hdpe_pellet', mold: '#thermal:crafting/dies/plate', output: 'contenttweaker:compressed_plate', input: '#forge:ingots/compressed_iron', mold: 'immersiveengineering:mold_plate', output: 'emendatusenigmatica:diamond_plate', input: '#forge:gems/diamond', mold: 'immersiveengineering:mold_plate', output: 'emendatusenigmatica:emerald_plate', input: '#forge:gems/emerald', mold: 'immersiveengineering:mold_plate'}
+        { output: 'mekanism:hdpe_sheet', input: 'mekanism:hdpe_pellet', mold: '#thermal:crafting/dies/plate', output: 'contenttweaker:compressed_plate', input: '#forge:ingots/compressed_iron', mold: 'immersiveengineering:mold_plate', output: 'emendatusenigmatica:diamond_plate', input: '#forge:gems/diamond', mold: 'immersiveengineering:mold_plate', output: 'emendatusenigmatica:emerald_plate', input: '#forge:gems/emerald', mold: 'immersiveengineering:mold_plate', output: 'woodenutilities:wooden_plate', input: '#minecraft:planks', mold: 'immersiveengineering:mold_plate', output: 'pneumaticcraft:compressed_iron_gear', input: '#forge:ingots/compressed_iron', mold: 'immersiveengineering:mold_gear'}
     ];
 
     recipes.forEach((recipe) => {
